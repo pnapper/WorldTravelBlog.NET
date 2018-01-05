27eff0ef130f7e406b0cd52d8e88e1e3d8b702b1
patrick napper <patrick@patricknapper.com>
2018-01-04 16:55:09 -0800

    add views and functionality for People class.

--- WorldTravelBlog/Controllers/PeopleController.cs
+++ WorldTravelBlog/Controllers/PeopleController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorldTravelBlog.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,61 @@ namespace WorldTravelBlog.Controllers
 {
     public class PeopleController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+		// GET: /<controller>/
+		private WorldTravelBlogContext db = new WorldTravelBlogContext();
+
+		public IActionResult Index()
         {
-            return View();
+            return View(db.People.ToList());
         }
+		public IActionResult Details(int id)
+		{
+			Person thisPerson = db.People.FirstOrDefault(names => names.PersonId == id);
+			return View(thisPerson);
+		}
+
+		public IActionResult Create()
+		{
+			ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "Name", "Description"); 
+            ViewBag.ExperienceId = new SelectList(db.Experiences, "ExperienceId", "Name", "Description");
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Person name)
+		{
+			db.People.Add(name);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Edit(int id)
+		{
+			var thisPerson = db.People.FirstOrDefault(names => names.PersonId == id);
+			return View(thisPerson);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Person person)
+		{
+			db.Entry(person).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var thisPerson = db.People.FirstOrDefault(names => names.PersonId == id);
+			return View(thisPerson);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var thisPerson = db.People.FirstOrDefault(names => names.PersonId == id);
+			db.People.Remove(thisPerson);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
     }
 }
