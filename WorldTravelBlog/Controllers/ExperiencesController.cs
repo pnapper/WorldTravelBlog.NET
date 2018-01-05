51babc6876782e45393d3e1bd9e5d6a9041c84a7
patrick napper <patrick@patricknapper.com>
2018-01-04 16:26:26 -0800

    add edit and delete functionality for Experiences.

--- WorldTravelBlog/Controllers/ExperiencesController.cs
+++ WorldTravelBlog/Controllers/ExperiencesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WorldTravelBlog.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,5 +19,55 @@ namespace WorldTravelBlog.Controllers
         {
             return View(db.Experiences.ToList());
         }
+
+		public IActionResult Details(int id)
+		{
+            var thisExperience = db.Experiences.Include(x=>x.Location).FirstOrDefault(names => names.ExperienceId == id);
+			return View(thisExperience);
+		}
+
+		public IActionResult Create()
+		{
+			ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "Name", "Description"); 
+            return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Experience item)
+		{
+			db.Experiences.Add(item);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Edit(int id)
+		{
+			var thisExperience = db.Experiences.FirstOrDefault(names => names.ExperienceId == id);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "Name", "Description");
+			return View(thisExperience);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Experience doggy)
+		{
+			db.Entry(doggy).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var thisExperience = db.Experiences.FirstOrDefault(names => names.ExperienceId == id);
+			return View(thisExperience);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var thisExperience = db.Experiences.FirstOrDefault(names => names.ExperienceId == id);
+			db.Experiences.Remove(thisExperience);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
     }
 }
